b2309bbf31934602dbd8794969ad27d120ae8b80
mikigal <mikigal.priv@gmail.com>
2026-04-17 23:45:35 +0200

    presence

--- zuna-server/ws/ws_presence_endpoint.go
+++ zuna-server/ws/ws_presence_endpoint.go
@@ -7,12 +7,17 @@ import (
 )
 
 // Receive over: presence
-type LastSeenUpdateRequest struct {
+// Response multicast over: presence_update
+type PresenceRequest struct {
 	Active bool `json:"active"`
 }
 
+type PresenceResponseMulticast struct {
+	Presence data.PresenceDTO `json:"presence"`
+}
+
 func (r *MessageRouter) handlePresence(c HubClient, msg IncomingMessage, userData data.UserData) {
-	var req LastSeenUpdateRequest
+	var req PresenceRequest
 	if err := json.Unmarshal(msg.Payload, &req); err != nil {
 		sendError(c, "bad_request", "bad request")
 		return
@@ -21,4 +26,22 @@ func (r *MessageRouter) handlePresence(c HubClient, msg IncomingMessage, userDat
 	userData.LastSeen = time.Now().UnixMilli()
 	userData.Active = req.Active
 	data.UpdateUserData(userData)
+
+	for _, ud := range data.UserDataMap {
+		if ud.ConnectionID == "" {
+			continue
+		}
+
+		if ud.UserID == userData.UserID {
+			continue
+		}
+
+		c.Send(OutgoingMessage{Type: "presence_update", Payload: PresenceResponseMulticast{
+			Presence: data.PresenceDTO{
+				UserID:   userData.UserID,
+				LastSeen: userData.LastSeen,
+				Active:   userData.Active,
+			},
+		}})
+	}
 }
