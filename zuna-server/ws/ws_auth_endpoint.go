20d290c2bf7bdc224e9875b5b0190d9c8ed913ac
mikigal <mikigal.priv@gmail.com>
2026-04-19 13:52:21 +0200

    require server password in websockets auth

--- zuna-server/ws/ws_auth_endpoint.go
+++ zuna-server/ws/ws_auth_endpoint.go
@@ -1,8 +1,14 @@
 package ws
 
 import (
+	"encoding/json"
 	"zuna-server/data"
+	"zuna-server/utils"
 )
+
+type AuthRequest struct {
+	ServerPassword string `json:"server_password"`
+}
 
 type AuthResponse struct {
 	Success string `json:"success"`
@@ -11,6 +17,17 @@ type AuthResponse struct {
 // Receive over: auth
 // Response to sender over: auth_confirmation
 func (r *MessageRouter) handleAuth(c HubClient, msg IncomingMessage, userData data.UserData) {
+	var req AuthRequest
+	if err := json.Unmarshal(msg.Payload, &req); err != nil {
+		sendError(c, "bad_request", "bad request")
+		return
+	}
+
+	if !utils.ValidateServerPassword(req.ServerPassword) {
+		sendError(c, "unauthorized", "invalid server password")
+		return
+	}
+
 	userData.ConnectionID = c.ID()
 	data.UpdateUserData(userData)
 	c.Send(OutgoingMessage{Type: "auth_confirmation", Payload: AuthResponse{
